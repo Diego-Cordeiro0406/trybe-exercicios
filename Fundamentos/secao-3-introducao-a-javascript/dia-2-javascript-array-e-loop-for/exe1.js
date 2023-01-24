8d7f93429d66ec3b66e42daa63084d5b73c3daa9
diego cordeiro <diegocordeiro121@gmail.com>
2023-01-24 15:40:02 -0300

    exercicios do dia

--- Fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exe1.js
+++ Fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exe1.js
@@ -1,16 +1,50 @@
 // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// console.log(numbers[0])
-// console.log(numbers[1])
-// console.log(numbers[2])
-// console.log(numbers[3])
-// console.log(numbers[4])
-// console.log(numbers[5])
-// console.log(numbers[6])
-// console.log(numbers[7])
-// console.log(numbers[8])
-// console.log(numbers[9])
-
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // for(let index = 0; index < numbers.length; index += 1){
 //     console.log(numbers[index])
 // }
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+// for(let index = 0; index < numbers.length; index += 1){
+    
+//     soma += numbers[index]
+// }
+//  console.log(soma)
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+// for(let index = 0; index < numbers.length; index += 1){
+    
+//     soma += numbers[index]
+// }
+// let media = soma / numbers.length;
+//  console.log(media)
+
+// console.log(media)
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+// for(let index = 0; index < numbers.length; index += 1){
+    
+//     soma += numbers[index]
+// }
+// let media = soma / numbers.length;
+// if(media >= 20){
+//     console.log("valor maior que 20")
+// }else if(media < 20){
+//     console.log("valor menor que 20")
+// }
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let impar = 0;
+//  for(let index = 0; index < numbers.length; index += 1){
+//     if(numbers[index] % 2 == 1){
+//         impar += 1
+//     }
+    
+// }
+// console.log("Foram encontrados " + impar + " números ímpar")
+// if(impar == 0){
+//     console.log("nenhum valor ímpar encontrado")
+//     }
+    
