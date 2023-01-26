5357b7e81875c3f49d7d999e0dc2c04bac887256
diego cordeiro <diegocordeiro121@gmail.com>
2023-01-26 15:34:50 -0300

    resolvendo primeiros exercicos

--- Fundamentos/secao-3-introducao-a-javascript/dia-4-objetos-e-funcoes/exe.js
+++ Fundamentos/secao-3-introducao-a-javascript/dia-4-objetos-e-funcoes/exe.js
@@ -1,13 +1,29 @@
-let player = {
-    nome: "Marta",
-    sobreNome: "Silva",
-    age: 34,
-    medalhas:["ouro: 2", "prata: 3"]
-}
+//let player = {
+//   nome: "Marta",
+//   sobreNome: "Silva",
+//   age: 34,
+//   medalhas:["ouro: 2", "prata: 3"]
+//}
 
 //console.log("A jogadora " + player.nome +" " + player.sobreNome + " tem " + player.age + " anos de idade")
 
 //player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
 //console.log("A jogadora " + player.nome +" " + player.sobreNome + " foi eleita a melhor do mundo por 6 vezes " + "sendo elas em " + player.bestInTheWorld)
 
-console.log("A jogadora possui " + player.medalhas)
+//console.log("A jogadora possui " + player.medalhas)
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  //console.log("Bem vinda, " + info.personagem)
+
+  info['recorrente'] = 'sim'
+  //console.log(info)
+
+//   for (let index in info){
+// console.log(index)
+//   }
+  
