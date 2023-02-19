cdf355780a3690d7c0393e386c41a39c54a5bb91
diego cordeiro <diegocordeiro121@gmail.com>
2023-02-19 16:27:42 -0300

    exercício do dia parte-4

---
+++ Fundamentos/secao-6-introducao-a-es6-e-testes-unitarios/dia-2-primeiros-passos-em-jest/techList.js
@@ -0,0 +1,17 @@
+function techList(newObj, nome) {
+let numSort = newObj.sort();
+let vetor = []
+if(newObj.length == 0) {
+    return 'Vazio!'
+}
+for(let i = 0; i < newObj.length; i += 1) {
+  vetor.push({
+    tech:newObj[i],
+    name:nome
+  })
+}
+return vetor;
+}
+
+console.log(techList(['react', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'))
+module.exports = techList;
