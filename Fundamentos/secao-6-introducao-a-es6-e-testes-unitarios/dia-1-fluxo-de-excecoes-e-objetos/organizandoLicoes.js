2f89d9e75f69015a738743938c190f1bf9dccf3d
diego cordeiro <diegocordeiro121@gmail.com>
2023-02-22 20:05:47 -0300

    exercicios do dia

---
+++ Fundamentos/secao-6-introducao-a-es6-e-testes-unitarios/dia-1-fluxo-de-excecoes-e-objetos/organizandoLicoes.js
@@ -0,0 +1,66 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+function addT(obj, key, value) {
+    obj[key] = value
+   
+  }
+
+  addT(lesson2, 'turno', 'noite');
+//   console.log(lesson2)
+
+function listKeys(obj) {
+ let keys = Object.keys(obj);
+ return keys
+}
+
+listKeys(lesson1)
+
+function objLength(obj) {
+    let keys = Object.keys(obj).length;
+    return keys
+   }
+   
+   objLength(lesson1)
+
+function listValues(obj) {
+  let values = Object.values(obj);
+
+  return values
+}
+
+listValues(lesson1)
+
+let allLessons = Object.assign({},{lesson1, lesson2, lesson3})
+
+function showStudents(obj) {
+  let students = 0;
+  const keys = Object.keys(obj);
+  for(i in keys) {
+    students += obj[keys[i]].numeroEstudantes
+  }
+  return students
+} 
+
+showStudents(allLessons);
+
+function getValueByNumber(obj, indice) {
+
+}
--- Fundamentos/secao-6-introducao-a-es6-e-testes-unitarios/dia-1-fluxo-de-excecoes-e-objetos/script.js
+++ Fundamentos/secao-6-introducao-a-es6-e-testes-unitarios/dia-1-fluxo-de-excecoes-e-objetos/script.js
@@ -1,28 +1,30 @@
-const paragrafo = document.getElementById('result');
-const value1 = document.getElementById('value1').value;
-const value2 = document.getElementById('value2').value;
+const inputIsValid = (value1, value2) => {
+  if (!value1 || !value2) {
+    throw new Error('Preencha os campos para realizar a soma');
+  }
+  if (Number.isNaN(Number(value1)) || Number.isNaN(Number(value2))) {
+    throw new Error('Informe dois números para realizar a soma');
+  }
+};
 function calculateSum() {
-  if (value1 === '' && value2 === '') throw new Error('Preencha os campos para realizar a soma');
-  if (value1 === '' || value2 === '') throw new Error('Preencha os campos para realizar a soma');
-  if (isNaN(value1) || isNaN(value2)) throw new Error('Informe dois números para realizar a soma');
-  const result = Number(value1) + Number(value2);
-  return result;
-}
-
-function displayResult(result) {
-  paragrafo.innerHTML = `Resultado: ${result}`;
-}
-
-function sum() {
   try {
-    const result = calculateSum();
-    displayResult(result);
-  } catch (errou) {
-    paragrafo.innerHTML = `${errou}`;
-  } finally {
+    const value1 = document.getElementById('value1').value;
+    const value2 = document.getElementById('value2').value;
+    inputIsValid(value1, value2);
+    const result = Number(value1) + Number(value2);
+    return result;
+  } catch (error) {
+    document.getElementById('result').innerHTML = `Erro: ${error.message}`;
+  }
+}
+function displayResult(result) {
+    document.getElementById('result').innerHTML = `Resultado: ${result}`;
     document.getElementById('value1').value = '';
     document.getElementById('value2').value = '';
-  }
+}
+function sum() {
+    const result = calculateSum();
+    if (result) displayResult(result);
 }
 window.onload = () => {
   const button = document.getElementById('button');
