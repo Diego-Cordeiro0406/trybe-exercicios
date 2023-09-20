a14b7c9378dfbe5c5a415cf55fe7cb83b6a4bf60
diego cordeiro <diegocordeiro121@gmail.com>
2023-09-20 14:30:58 -0300

    exercicios do dia

---
+++ Back-end/secao-8-programacao-orientada-a-objetos/dia-4-introducao-aos-principios-s-o-d/exercises/src/models/interfaces/index.ts
@@ -0,0 +1,14 @@
+export { IPlant, INewPlant } from './IPlant';
+export {
+  IModel,
+  IModelReader,
+  IModelWriter,
+  IModelDelete,
+} from './IModel';
+export { default as IOpsInfo } from './IOpsInfo';
+export {
+  IService,
+  IServiceReader,
+  IServiceWriter,
+  IServiceDelete,
+} from './IService';
