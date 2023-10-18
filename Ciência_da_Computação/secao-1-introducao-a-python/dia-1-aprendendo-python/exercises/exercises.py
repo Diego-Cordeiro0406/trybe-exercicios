3a746d297d92e3fde081b8446916202ff645617e
diego cordeiro <diegocordeiro121@gmail.com>
2023-10-18 08:04:38 -0300

    exercicios do dia - parte 2

--- Ciência_da_Computação/secao-1-introducao-a-python/dia-1-aprendendo-python/exercises/exercises.py
+++ Ciência_da_Computação/secao-1-introducao-a-python/dia-1-aprendendo-python/exercises/exercises.py
@@ -48,6 +48,50 @@ def type_of_triangle(side1, side2, side3):
         return "escaleno"
 
 
+# Exercicio 7
+def ret_smaller(li):
+    smaller = 0
+    for index in li:
+        smaller += index
+    for value in li:
+        if value < smaller:
+            smaller = value
+    return smaller
+
+
+# Exercicio 8
+def printTriangle(value):
+    counter = 1
+    while counter <= value:
+        print(counter * '*')
+        counter += 1
+
+
+# Exercicio 9
+def sum_of_all(value):
+    return sum(range(1, value + 1))
+
+
+def calculate_fuel(liters, type):
+    if type == "A":
+        price = 1.90
+        discount = 0.03
+    if liters > 20:
+        discount = 0.05
+    elif type == "G":
+        price = 2.50
+        discount = 0.04
+    if liters > 20:
+        discount = 0.06
+    total = price * liters
+    total -= total * discount
+    return total
+
+
+# print(calculate_fuel(20, 'A'))
+# printTriangle(5)
+# print(sum_of_all(5))
+# print(ret_smaller([1, 5, 9, 3, 19, 70, 8, 100, 2, 35, 27]))
 # print(
 #     ret_bigger_chart(
 # ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"]
