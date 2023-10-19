20c96dd4559b588341a3c5022a4dd93f153b4671
diego cordeiro <diegocordeiro121@gmail.com>
2023-10-19 11:37:39 -0300

    exercicios do dia

---
+++ Ciência_da_Computação/secao-1-introducao-a-python/dia-2-entrada-e-saida-de-dados-e-testes/examples/fix.py
@@ -0,0 +1,18 @@
+# user_name = input('digite seu nome:')
+
+# split_name = [letra for letra in user_name]
+
+# for index in split_name:
+#     print(index)
+
+
+# numbers = input('digite seus números separando com espaços: ')
+
+# split_numbers = numbers.split(" ")
+
+# total = 0
+
+# for number in split_numbers:
+#     total += int(number)
+
+# print(total)
