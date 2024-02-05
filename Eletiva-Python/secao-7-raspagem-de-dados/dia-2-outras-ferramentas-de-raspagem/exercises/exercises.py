dd69ea1a83039691bd10d75958437c1fd12dd589
Diego Cordeiro <diegocordeiro121@gmail.com>
2024-02-05 16:40:13 -0300

    exercicios do dia(parte 2)

--- Eletiva-Python/secao-7-raspagem-de-dados/dia-2-outras-ferramentas-de-raspagem/exercises/exercises.py
+++ Eletiva-Python/secao-7-raspagem-de-dados/dia-2-outras-ferramentas-de-raspagem/exercises/exercises.py
@@ -3,16 +3,28 @@ from selenium.webdriver.common.by import By
 
 firefox = webdriver.Firefox()
 
-response = firefox.get("https://quotes.toscrape.com/")
+# response = firefox.get("https://quotes.toscrape.com/")
 
 # exercise 1
 
 # title = firefox.find_element(By.CLASS_NAME, "quote")
 # print(title.find_element(By.TAG_NAME, "span").get_attribute("innerHTML"))
 
-# exercises 2
+# exercise 2
 
-paragraphs = firefox.find_elements(By.TAG_NAME, "p")
+# paragraphs = firefox.find_elements(By.TAG_NAME, "p")
 
-for paragraph in paragraphs:
-    print(paragraph.get_attribute("innerHTML"))
+# for paragraph in paragraphs:
+#     print(paragraph.get_attribute("innerHTML"))
+
+# exercise 3
+
+response = firefox.get("https://diolinux.com.br/")
+
+posts = firefox.find_elements(By.CLASS_NAME, "post-title")
+
+for post in posts:
+    attr = post.find_element(By.TAG_NAME, "a")
+    title = attr.get_attribute("innerHTML")
+    link = attr.get_attribute("href")
+    print(title, link)
